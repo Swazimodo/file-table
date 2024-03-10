a9a556f527241a260775918a1302eedcfd3882b4
Swazimodo <swn@duck.com>
2024-03-10 16:04:15 -0600

    feat: extract app context provider and test it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { FC, useContext } from 'react';
-
-import { ToastContextProvider, ToastMessageEmitter, toastContext } from 'components/toast';
 import { ErrorBoundary } from 'components/errorBoundary'
+import { AppContextProviders } from 'appContextProviders';
+import { ToastMessageEmitter } from 'components/toast';
 import { FileView } from 'fileView';
 
 
@@ -9,30 +8,13 @@ function App() {
   return (
     <div className="App">
       <ErrorBoundary>
-        <ToastContextProvider>
-          <RenderWhenReady>
-            <ToastMessageEmitter />
-            <FileView />
-          </RenderWhenReady>
-        </ToastContextProvider>
+        <AppContextProviders>
+          <ToastMessageEmitter />
+          <FileView />
+        </AppContextProviders>
       </ErrorBoundary>
     </div>
   );
-}
-
-interface WithChildrenProps {
-  children?: React.ReactNode
-}
-
-// This will not render the children until all context values are truthy
-// this will prevent the whole app from needing to do null checks
-const RenderWhenReady: FC<WithChildrenProps> = (props) => {
-  const { children } = props
-  const toastCtx = useContext(toastContext)
-  if (!toastCtx) {
-    return null
-  }
-  return <>{children}</>
 }
 
 export default App;
--- src/__tests__/unit/App.test.tsx
+++ src/__tests__/unit/App.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from 'App';
 
-test('renders learn react link', () => {
+// renderAppContextProviders_contextIsNull_shouldNotRenderChildren
+test('renderApp_initialRender_shouldSucceedWithoutRaisingException', () => {
   const { container } = render(<App />);
   const appWrapperElement = container.getElementsByClassName("App")
   expect(appWrapperElement[0]).toBeInTheDocument();
